6567ef15918187d84f8ffbc2a18d58a6197d14da
Mike Quinn <mike.quinn@roosterteeth.com>
2017-07-17 19:50:35 -0500

    fix bearer regex(again)

--- lib/rt_oauth_client/bearer_token.rb
+++ lib/rt_oauth_client/bearer_token.rb
@@ -5,7 +5,7 @@ module RtOauthClient
     end
 
     def find_bearer_token
-      if request.headers['HTTP_AUTHORIZATION'].match(RtOauthClient.configuration.bearer_token_regex)
+      if request&.headers['HTTP_AUTHORIZATION']&.match(RtOauthClient.configuration.bearer_token_regex)
         request.headers['HTTP_AUTHORIZATION'].split(/bearer/i).last.gsub(/\s/, '')
       end
     end
